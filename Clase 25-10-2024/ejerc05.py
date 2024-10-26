7c7e3d2008833cf6cbd1f0b16970b05921770b73
Marcos <marcoslopez11072006@gmail.com>
2024-10-26 10:19:27 -0300

    ejerc05

---
+++ Clase 25-10-2024/ejerc05.py
@@ -0,0 +1,15 @@
+"""Ejercicio 5
+Escriba un programa que genere una lista de numeros aleatorios entre 1y 100
+"""
+
+def aleatorio():
+    import random
+    return random.randint(1, 10)
+lista = [aleatorio() for i in range(10)]
+print(lista)
+
+s = set(lista)
+print(s)
+s.add(11)
+s.add(2)
+print(s)
