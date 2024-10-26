fa52efc635b060005e7bfbfe299a7c7b034c5625
Marcos <marcoslopez11072006@gmail.com>
2024-10-26 10:18:37 -0300

    ejerc03.py

---
+++ Clase 25-10-2024/ejerc03.py
@@ -0,0 +1,10 @@
+"""Ejercicio 3
+Escriba un programa que """
+nro1 = int(input("Escriba un nro:"))
+nro = int(input("Escriba un nro:"))
+print(max(nro1,nro))
+
+if nro1 > nro:
+    print(f"El mayor es: {nro1} ")
+else:
+    print(f"El mayor es: {nro}")
