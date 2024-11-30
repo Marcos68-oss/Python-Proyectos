fe3cfc90df9257b9c329ddefd3782bfb1df4dad8
Marcos <marcoslopez11072006@gmail.com>
2024-11-30 17:39:26 -0300

    Ejercicio 01

---
+++ Clase 22-11-2024/ejer01.py
@@ -0,0 +1,8 @@
+"""
+Comprueba si una edad almacenada en una variable es mayor de edad, es decir si su valor es mayor a 18. Si es asi, imprime el texto eres mayor de edad, en caso contrario impreime el texto NO eres mayor de edad
+"""
+edad = 18
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("NO eres mayor de edad")
