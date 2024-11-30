0b70a06a2ef359c3dbce53e72658f05fc1cce0da
Marcos <marcoslopez11072006@gmail.com>
2024-11-30 17:40:18 -0300

    Ejercicio 06

---
+++ Clase 22-11-2024/ejer06.py
@@ -0,0 +1,7 @@
+"""
+Ejercicio 06
+Dada una lista con nombre y dos apellidos, imprime solo los dos apellidos utilizando índices
+"""
+li = ["Ana", "Dure", "Diaz"]
+print(li[1:])
+print(li[1], li[2])
