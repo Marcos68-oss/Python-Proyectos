7aef99b90c7f5ed43615c85b62c8446822bf7014
Marcos <marcoslopez11072006@gmail.com>
2024-11-30 17:39:48 -0300

    Ejercicio 04

---
+++ Clase 22-11-2024/ejer04.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 04
+Dada una lista con diferentes notas, se debe imprimir la más baja y la más alta
+"""
+notas = [1,4,5,7,7,4,4]
+print(notas)
+print(f"La nota mas alta es {max(notas)}")
+print(f"La nota mas baja es {min(notas)}")
