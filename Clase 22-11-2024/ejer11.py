98927e430ee7b933558797b46d56c74a2eb0e2d5
Marcos <marcoslopez11072006@gmail.com>
2024-11-30 17:41:02 -0300

    Ejercicio 11

---
+++ Clase 22-11-2024/ejer11.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 11
+Imprimir los árboles de la lista hasta que aparezca nogal.
+Utiliza la variable arboles = ["manzano", "pino", "madroño", "eucalipto","nogal", "olivo", "almendro"]
+"""
+arboles = ["manzano", "pino", "mandroño", "eucalipto", "nogal", "olivo", "almendro"]
+
+for i in arboles:
+    print(i)
+    if i == "nogal":
+        break
