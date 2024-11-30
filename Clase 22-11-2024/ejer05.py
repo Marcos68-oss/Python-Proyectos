784ac61cb72a643f773e0e7e070550c094c7947f
Marcos <marcoslopez11072006@gmail.com>
2024-11-30 17:40:11 -0300

    Ejercicio 05

---
+++ Clase 22-11-2024/ejer05.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 05
+Comprobar si la fruta cereza se encuentra en una lista de frutas. En caso afirmativo, imprimir por pantalla el texto Cereza SÍ está en la lista. En caso contrario, imprimir por
+pantalla el texto Cereza NO está en la lista.
+"""
+frutas = ["manzana", "kiwi", "uva", "pera", "naranja", "pomelo", "cereza"]
+
+if "cereza" in frutas:
+    print("Cereza Si esta en la lista")
+else:
+    print("Cereza no esta en la lista")
